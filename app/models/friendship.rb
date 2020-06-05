6b5e5e1150e12a17663012b09ad584bb55056165
Kubilay <kblycaglayan@gmail.com>
2020-06-05 13:49:22 +0300

    Associations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,3 +1,5 @@
 class Friendship < ApplicationRecord
-  belongs_to :user
+  belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
+
+  scope :confirmed, -> { where(confirmed: true) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,7 +8,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-names = ["Kubilay","Tayo","Berry", "Dennis"]
+names = %w[Kubilay Tayo Berry Dennis]
 
 names.each do |name|
   User.create(name: name)
